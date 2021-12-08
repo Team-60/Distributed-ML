8f80f9a01fc94246b3bab29b3adf59d3b93a0a89
Rupanshu Yadav <58172798+zyrch@users.noreply.github.com>
2021-12-08 22:04:11 +0530

    serial model added

--- main.py
+++ main.py
@@ -6,18 +6,24 @@ import torch.nn as nn
 import numpy as np
 from tqdm import tqdm
 import argparse
+from train import *
 
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 parser = argparse.ArgumentParser(description='Shared Memory MNIST')
 parser.add_argument('--batch-size', type=int, default=64, help='batch size for training (default: 64)')
+parser.add_argument('--num-epochs', type=int, default=100, help='number of epochs for training (default: 100)')
+parser.add_argument('--momentum', type=int, default=0.9, help='momentum value of optimizer training (default: 0.9)')
+parser.add_argument('--learning-rate', type=int, default=0.001, help='learning rate of optimizer training (default: 0.001)')
+parser.add_argument('--display-interval', type=int, default=5, help='interval for printing while training each epoch')
 args = parser.parse_args()
 
 train_set = torchvision.datasets.CIFAR10('./data', download=True, train=True, transform=transforms.Compose([transforms.ToTensor()]))
 test_set = torchvision.datasets.CIFAR10("./data", download=True, train=False, transform=transforms.Compose([transforms.ToTensor()])) 
 
-class Net(nn.Module):
+class CNN(nn.Module):
+
   def __init__(self):
     super().__init__()
     self.conv = nn.Sequential(
@@ -42,5 +48,16 @@ class Net(nn.Module):
     return out
 
 if __name__ == '__main__':
-    pass
+
+    model = CNN()
+    model.to(device)
+
+    dataloader_kwargs = {
+            'batch_size': args.batch_size,
+            'shuffle': True,
+    }
+
+    train(args, model, device, train_set, dataloader_kwargs)
+    test(args, model, device, test_set, dataloader_kwargs)
+
 
---
+++ train.py
@@ -0,0 +1,52 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+
+def train(args, model, device, dataset, dataloader_kwargs):
+    
+    train_loader = torch.utils.data.DataLoader(dataset, **dataloader_kwargs)
+    error = nn.NLLLoss()
+
+    optimizer = optim.SGD(model.parameters(), momentum=args.momentum, lr=args.learning_rate)
+    for epoch in range(args.num_epochs):
+        epoch_loss = 0
+        for idx, (images, labels) in enumerate(train_loader):
+
+            optimizer.zero_grad()
+            images, labels = images.to(device), labels.to(device)
+            output = model(images)
+
+            loss = error(output, labels)
+            epoch_loss += loss
+
+            loss.backward()
+            optimizer.step()
+
+            if idx % args.display_interval == 0:
+                print('Trian Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:0.6f}'.format(epoch, idx * len(images), len(train_loader.dataset),
+                    100 * idx / len(train_loader), loss.item()))
+
+
+def test(args, model, device, dataset, dataloader_kwargs):
+    
+    test_loader = torch.utils.data.DataLoader(dataset, **dataloader_kwargs)
+    error = nn.NLLLoss(reduction='sum')
+
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for idx, (images, labels) in enumerate(test_loader):
+            images, labels = images.to(device), labels.to(device)
+            output = model(images)
+
+            loss = error(output, labels)
+            test_loss = loss.item()
+            pred = output.max(1)[1]
+            correct += pred.eq(labels.to(device)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    print('Test set: Average Loss {:.2f}, Accuracy: {:.2f}%'.format(
+        test_loss, correct / len(test_loader.dataset) * 100))
