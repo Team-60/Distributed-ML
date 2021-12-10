d542bec31c2a1927a6a505d10c0005ac9b354844
watch24hrs-iiitd <divyansh19464@iiitd.ac.in>
2021-12-10 17:23:56 +0530

    update: downloader

--- Local-SGD/downloader.py
+++ Local-SGD/downloader.py
@@ -1,6 +1,5 @@
-import torch
 import torchvision
 import torchvision.transforms as transforms
 
-train_set = torchvision.datasets.CIFAR10('./data', download=True, train=True, transform=transforms.Compose([transforms.ToTensor()]))
-test_set = torchvision.datasets.CIFAR10("./data", download=True, train=False, transform=transforms.Compose([transforms.ToTensor()])) 
+torchvision.datasets.CIFAR10('./data', download=True, train=True, transform=transforms.Compose([transforms.ToTensor()]))
+torchvision.datasets.CIFAR10("./data", download=True, train=False, transform=transforms.Compose([transforms.ToTensor()])) 
