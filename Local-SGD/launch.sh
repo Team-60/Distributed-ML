ab452579ec39ad898422dff725f02cfc20a3ff13
Rupanshu Yadav <58172798+zyrch@users.noreply.github.com>
2021-12-10 16:31:10 +0530

    bash script local SGD added

--- Local-SGD/launch.sh
+++ Local-SGD/launch.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-python3 main.py --world-size 2 --rank 0
-python3 main.py --world-size 2 --rank 1
+python3 main.py --world-size 3 --rank 0 & pid=$!
+PID_LIST+=" $pid";
+python3 main.py --world-size 3 --rank 1 & pid=$!
+PID_LIST+=" $pid";
+python3 main.py --world-size 3 --rank 2 & pid=$!
+PID_LIST+=" $pid";
+
+trap "kill $PID_LIST" SIGINT
+
+echo "Parallel processes have started";
+
+wait $PID_LIST
+
+echo
+echo "All processes have completed";
 
 
